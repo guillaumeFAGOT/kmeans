62c7fa2bea57f0de704b765ba3291d7da1f88df7
guillaume <fagotgnantes@gmail.com>
2021-10-19 16:13:02 +0200

    Fabrication des ellipses de Kmeans

--- Script.R
+++ Script.R
@@ -7,3 +7,9 @@ mtcars_num <- mtcars %>%
 str(mtcars_num)
 mtcars_num_sc <- scale(mtcars_num)
 str(mtcars_num_sc)
+
+km.out2 = kmeans(mtcars_num_sc,centers=2,nstart =20) 
+km.out2$cluster
+
+library(factoextra)
+fviz_cluster(km.out2, mtcars_num_sc, ellipse.type = "norm") 
